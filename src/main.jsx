f747c9bbe70ea903853b699261f053c2779369c2
dominionbanjo <dominionbanjo@gmail.com>
2024-06-04 19:25:55 +0100

    Added login and signup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Menu from './Components/Menu/Menu.jsx'
-import About from './Components/About/About.jsx'
-import './index.css'
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Menu from "./Components/Menu/Menu.jsx";
+import About from "./Components/About/About.jsx";
+import Login from "./Components/Login/Login.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signup from "./Components/Signup/Signup.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -18,9 +20,17 @@ const router = createBrowserRouter([
     path: "about",
     element: <About />,
   },
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "signup",
+    element: <Signup />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
